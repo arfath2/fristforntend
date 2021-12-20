c98c63254e961936369d14383f049ddd726f2686
arfath2 <arfath23mohammed@gmail.com>
2021-12-20 21:49:52 +0530

    index.html

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ headerTitle.style.borderBottom = '3px solid black';
 var Title = document.getElementById('add');
 Title.innerHTML = '<b>ADD ITEMS</b>'
 Title.style.color = 'green';
+
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green'
+
+for(var i=0;i<=items.length;i++){
+  items[i].style.fontWeight='bold'
+}
