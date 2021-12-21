aa56cb2fad8695c13b75338b6f7e0a8d08fd5432
arfath2 <arfath23mohammed@gmail.com>
2021-12-21 20:46:23 +0530

    queryselectorall

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.style.borderBottom = '3px solid black';
-var Title = document.getElementById('add');
-Title.innerHTML = '<b>ADD ITEMS</b>'
-Title.style.color = 'green';
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.style.borderBottom = '3px solid black';
+// var Title = document.getElementById('add');
+// Title.innerHTML = '<b>ADD ITEMS</b>'
+// Title.style.color = 'green';
 
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green'
+// var items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'green'
 
-// for(var i=0;i<=items.length;i++){
-//   items[i].style.fontWeight='bold'
-// }
+//  for(var i=0;i<=items.length;i++){
+//    items[i].style.fontWeight='bold'
+//  }
 
-var lists = document.getElementsByTagName('li');
-console.log(lists)
-lists[1].style.backgroundColor = 'red'
+// var lists = document.getElementsByTagName('li');
+// console.log(lists)
+// lists[1].style.backgroundColor = 'red'
+
+var lists = document.querySelector('.list-group-item:nth-child(2)');
+lists.style.backgroundColor = 'green'
+
+var list = document.querySelector('.list-group-item:nth-child(3)');
+list.style.display = 'none'
+
+var item = document.querySelectorAll('li:nth-child(2)');
+item.style.backgroundColor = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<=odd.length;i++){
+odd[i].style.backgroundColor = 'green';
+}
