2feeffa0ffff75a08f1d5a27e41037a1e50761e6
arfath2 <arfath23mohammed@gmail.com>
2021-12-22 07:11:33 +0530

    'filter'

--- index.js
+++ index.js
@@ -93,3 +93,19 @@ function removeItem(e){
     }
   }
 }
+
+
+var filter = document.getElementById('filter');
+filter.addEventListener('keyup', filterItems);
+function filterItems(e){
+  var text = e.target.value.toLowerCase();
+  var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
